13b9b464c7b3d1d559c225950244b282b5a9fdac
nitin <nitin.kumar@digimantra.com>
2025-01-21 12:42:00 +0530

    Latest Changes

---
+++ cypress/fixtures/user.js
@@ -0,0 +1,19 @@
+[
+    {
+      "username": "lol@yopmail.com ",
+      "password": "Lol@123"
+    },
+    {
+        "username": "nitin@yopmail.com",
+        "password": "Nitin@123"
+    },
+    {
+        "username": "nitin12@yopmail.com",
+        "password": "Nitin@123"
+    },
+    {
+        "username": "arti@yopmail.com",
+        "password": "Arti@123"
+    }
+  ]
+ 
